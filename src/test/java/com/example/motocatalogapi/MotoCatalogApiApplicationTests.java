5b2411a6ac0fb8ea4b5ceec975c14ce501f30fea
laurence <laurencemiguel1@gmail.com>
2026-07-28 22:57:36 -0300

    feat: dependencias de seguranca e jwt

--- src/test/java/com/example/motocatalogapi/MotoCatalogApiApplicationTests.java
+++ src/test/java/com/example/motocatalogapi/MotoCatalogApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MamotoApiApplicationTests {
+class MotoCatalogApiApplicationTests {
 
 	@Test
 	void contextLoads() {
